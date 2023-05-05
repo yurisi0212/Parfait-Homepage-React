51be163a70d28ebe50f199c936f8a3d2028bc484
yurisi0212 <Dev.yurisi@gmail.com>
2023-05-06 01:43:31 +0900

    トップページを完成

--- src/routes/about.js
+++ src/routes/about.js
@@ -1,13 +1,52 @@
 import Navigation from '../navigation'
+import Slider from "react-slick";
+import Container from 'react-bootstrap/Container';
 
 function About() {
-    return (
-      <div>
-        <Navigation />
+  const settings = {
+    autoplay: true,
+    autoplaySpeed: 2500,
+    infinite: true,
+    dots: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+
+  return (
+    <div>
+      <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+      <Navigation />
+      <Container>
         <h2>About</h2>
-      </div>
-    
-    );
-  }
-  
-  export default About;
+        <div>
+          <Slider {...settings}>
+            <div>
+              <img src="/images/Valorant.png" className='w-100' />
+            </div>
+            <div>
+              <img src="/images/minecraft.png" className='w-100' />
+            </div>
+            <div>
+              <img src="/images/oekaki.png" className='w-100' />
+            </div>
+            <div>
+              <img src="/images/Siege.png" className='w-100' />
+            </div>
+            <div>
+              <img src="/images/GTA.png" className='w-100' />
+            </div>
+            <div>
+              <img src="/images/Apex.png" className='w-100' />
+            </div>
+          </Slider>
+        </div>
+      </Container>
+    </div>
+
+  );
+}
+
+export default About;
--- src/routes/top.js
+++ src/routes/top.js
@@ -1,53 +1,20 @@
 import Container from 'react-bootstrap/Container';
-import Slider from "react-slick";
 import Navigation from '../navigation'
+import { Helmet } from "react-helmet";
 
 function Top() {
-    const settings = {
-        autoplay: true,
-        autoplaySpeed: 2500,
-        infinite: true,
-        dots: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-    };
-
     return (
         <div>
             <Navigation />
-            <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
             <div id='index' className='text-center'>
-                <Container className='bg-light p-3'>
-                    <div className='mt-2'>
-                        <h1>ようこそ</h1>
-                        <h2>Welcome To Parfait Homepage!</h2>
-                    </div>
+                <Helmet>
+                    <link rel="stylesheet" href="top.css" />
+                </Helmet>
+                <div className='top-container'>
                     <div>
-                        <Slider {...settings}>
-                            <div>
-                                <img src="/images/Valorant.png" className='w-100' />
-                            </div>
-                            <div>
-                                <img src="/images/minecraft.png" className='w-100' />
-                            </div>
-                            <div>
-                                <img src="/images/oekaki.png" className='w-100' />
-                            </div>
-                            <div>
-                                <img src="/images/Siege.png" className='w-100' />
-                            </div>
-                            <div>
-                                <img src="/images/GTA.png" className='w-100' />
-                            </div>
-                            <div>
-                                <img src="/images/Apex.png" className='w-100' />
-                            </div>
-                        </Slider>
+                        <h3>Welcome To Parfait Homepage!</h3>
                     </div>
-                </Container>
+                </div>
             </div>
         </div>
     );
